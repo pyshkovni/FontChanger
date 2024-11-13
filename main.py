7a052d2d8be1ae56d48f1ee38df6687523762a82
student-tets <guess.the.student@gmail.com>
2024-11-13 13:34:49 +0300

    Добавил импорты в main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 ## main стартовый модуль проекта
 
-# TODO - import <зависимость из задания>
+# TODO - переименовать функции
+from functions import func1, func2
 
 def main():
     # TODO - сделай вызов функций из functions
